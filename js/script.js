54fb3d0cf915a29dea8022678cdc6577dbe05286
Calucata <calucatacrt@gmail.com>
2026-08-31 21:36:21 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,2 @@
 
+document.querySelectorAll('[data-cta]').forEach(el=>el.addEventListener('click',()=>{if(window.fbq)fbq('track','InitiateCheckout',{content_name:'El Código de la Primera Impresión',placement:el.dataset.cta})}));
